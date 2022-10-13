35f7382d0b1bf02c1735b22d6011087c18779cdb
Fabrício Pinto Ferreira <pferreirafabricio@gmail.com>
2022-10-12 21:21:40 -0300

    feat: add pin animation

--- Assets/Scripts/Ui/Pin.cs
+++ Assets/Scripts/Ui/Pin.cs
@@ -7,14 +7,33 @@ public class Pin : MonoBehaviour
     [SerializeField]
     private float TimeToHide = 3;
 
+    [SerializeField]
+    private float TimeToDestroy = 15;
+
+    private Animator Animator;
 
     private float TimeBeingDisplayed = 0;
+
+    void Awake()
+    {
+        Animator = GetComponent<Animator>();
+
+        Animator.SetBool("Blinking", true);
+    }
+
+    void Start()
+    {
+        Destroy(gameObject, TimeToDestroy);
+    }
 
     void Update()
     {
         TimeBeingDisplayed += Time.deltaTime;
 
         if (TimeBeingDisplayed >= TimeToHide)
+        {
             gameObject.SetActive(false);
+            Animator.SetBool("Blinking", false);
+        }
     }
 }
